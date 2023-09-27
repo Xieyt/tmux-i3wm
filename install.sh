54dcfc5c03a9482f64b2d51a0bf8cf0047cb1f9c
Xieyt <alokmilenium@gmail.com>
2023-09-28 02:33:58 +0530

    Added: Custom Config

---
+++ install.sh
@@ -0,0 +1,24 @@
+tmux_dir="/home/${USER}/.tmux-alok"
+tmux_link="/home/${USER}/.tmux.conf.alok"
+git clone https://github.com/Xieyt/tmux-i3wm "${tmux_dir}"
+
+if [[ -d "$tmux_dir" ]]; then
+    ln -sfn "$tmux_dir/.tmux.conf" "$tmux_link"
+fi
+
+new_alias="alias xtmux='tmux -f ~/.tmux.conf.alok -S /tmp/tmux-alok/default '"
+
+if [ -n "$BASH_VERSION" ]; then
+    if ! grep -qF "$new_alias" ~/.bashrc; then
+    # Append the alias to the Bash configuration file (e.g., ~/.bashrc)
+        echo "$new_alias" >> ~/.bashrc
+    fi
+fi
+
+# Check if the current shell is Zsh
+if [ -n "$ZSH_VERSION" ]; then
+    if ! grep -qF "$new_alias" ~/.zshrc; then
+    # Append the alias to the Zsh configuration file (e.g., ~/.zshrc)
+        echo "$new_alias" >> ~/.zshrc
+    fi
+fi
